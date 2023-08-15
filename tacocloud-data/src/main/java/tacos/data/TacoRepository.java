ff305ef7c01797ddb8541b3854ccd402f6b67436
jhcode33 <jhcode33@gmail.com>
2023-08-16 00:00:00 +0900

    Add TacoRepo, findById, save

--- tacocloud-data/src/main/java/tacos/data/TacoRepository.java
+++ tacocloud-data/src/main/java/tacos/data/TacoRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 
 import tacos.Taco;
 
+import java.util.Optional;
+
 
 public interface TacoRepository 
          extends PagingAndSortingRepository<Taco, Long> {
 
+    Optional<Taco> findById(Long id);
+    Taco save(Taco taco);
 }
