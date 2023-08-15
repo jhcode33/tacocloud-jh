b2999616693d00094554b0c915d967d30fc974dd
jhcode33 <jhcode33@gmail.com>
2023-08-16 00:00:00 +0900

    complete tacocloud-web

--- tacocloud-web/src/main/java/tacos/web/OrderProps.java
+++ tacocloud-web/src/main/java/tacos/web/OrderProps.java
@@ -1,7 +1,7 @@
 package tacos.web;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import org.springframework.boot.context.properties.
                                         ConfigurationProperties;
 import org.springframework.stereotype.Component;
