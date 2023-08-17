94ee5c07a7e85274ffd1cb139f15649b5ae976f2
jhcode33 <jhcode33@gmail.com>
2023-08-17 16:35:00 +0900

    complete SecurityConfig

--- tacocloud-security/src/main/java/tacos/security/SecurityConfig.java
+++ tacocloud-security/src/main/java/tacos/security/SecurityConfig.java
@@ -3,78 +3,126 @@ package tacos.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation
-             .authentication.builders.AuthenticationManagerBuilder;
+        .authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web
-             .builders.HttpSecurity;
+        .builders.HttpSecurity;
 import org.springframework.security.config.annotation.web
-                        .configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web
-                        .configuration.WebSecurityConfigurerAdapter;
+        .configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-  
+public class SecurityConfig {
+
   @Autowired
   private UserDetailsService userDetailsService;
-  
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-	  
-    http
-      .authorizeRequests()
-        .antMatchers(HttpMethod.OPTIONS).permitAll() // needed for Angular/CORS
-        .antMatchers("/design", "/orders/**")
-            .permitAll()
-            //.access("hasRole('ROLE_USER')")
-        .antMatchers(HttpMethod.PATCH, "/ingredients").permitAll()
-        .antMatchers("/**").access("permitAll")
-        
-      .and()
-        .formLogin()
-          .loginPage("/login")
-          
-      .and()
-        .httpBasic()
-          .realmName("Taco Cloud")
-          
-      .and()
-        .logout()
-          .logoutSuccessUrl("/")
-          
-      .and()
-        .csrf()
-          .ignoringAntMatchers("/h2-console/**", "/ingredients/**", "/design", "/orders/**")
 
-      // Allow pages to be loaded in frames from the same origin; needed for H2-Console
-      .and()  
-        .headers()
-          .frameOptions()
-            .sameOrigin()
-      ;
+  @Autowired
+  @Lazy
+  private PasswordEncoder encoder;
+
+  @Bean
+  @Lazy
+  public PasswordEncoder encoder() {
+    //  return new StandardPasswordEncoder("53cr3t");
+    return NoOpPasswordEncoder.getInstance();
+  }
+
+  public AuthenticationManager authenticationManager(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
+    return auth.build();
   }
 
   @Bean
-  public PasswordEncoder encoder() {
-//    return new StandardPasswordEncoder("53cr3t");
-    return NoOpPasswordEncoder.getInstance();
-  }
-  
-  
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth)
-      throws Exception {
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+          http
+            .authorizeHttpRequests(authorize -> authorize
+                    .requestMatchers(HttpMethod.OPTIONS).permitAll() // needed for Angular/CORS
+                    .requestMatchers("/design", "/orders/**").permitAll()
+                    .requestMatchers(HttpMethod.PATCH, "/ingredients").permitAll()
+                    .requestMatchers("/**").permitAll()
+            )
+            .formLogin(formLogin -> formLogin
+                    .loginPage("/login")
+            )
+            .httpBasic(httpBasic -> httpBasic
+                    .realmName("Taco Cloud")
+            )
+            .logout(logout -> logout
+                    .logoutSuccessUrl("/")
+            )
+            .csrf(csrf -> csrf
+                    .ignoringRequestMatchers("/h2-console/**", "/ingredients/**", "/design", "/orders/**")
+            )
+            .headers(headers -> headers
+                    .frameOptions(frameOptions -> frameOptions
+                            .sameOrigin()
+                    )
+            );
 
-    auth
-      .userDetailsService(userDetailsService)
-      .passwordEncoder(encoder());
-    
+    return http.build();
   }
+
+//  @Override
+//  protected void configure(HttpSecurity http) throws Exception {
+//
+//    http
+//      .authorizeRequests()
+//        .requestMatchers(HttpMethod.OPTIONS).permitAll() // needed for Angular/CORS
+//        .requestMatchers("/design", "/orders/**")
+//            .permitAll()
+//            //.access("hasRole('ROLE_USER')")
+//        .requestMatchers(HttpMethod.PATCH, "/ingredients").permitAll()
+//        .requestMatchers("/**").access("permitAll")
+//
+//      .and()
+//        .formLogin()
+//          .loginPage("/login")
+//
+//      .and()
+//        .httpBasic()
+//          .realmName("Taco Cloud")
+//
+//      .and()
+//        .logout()
+//          .logoutSuccessUrl("/")
+//
+//      .and()
+//        .csrf()
+//          .ignoringRequestMatchers("/h2-console/**", "/ingredients/**", "/design", "/orders/**")
+//
+//      // Allow pages to be loaded in frames from the same origin; needed for H2-Console
+//      .and()
+//        .headers()
+//          .frameOptions()
+//            .sameOrigin()
+//      ;
+//  }
+
+//  @Bean
+//  public PasswordEncoder encoder() {
+////    return new StandardPasswordEncoder("53cr3t");
+//    return NoOpPasswordEncoder.getInstance();
+//  }
+
+
+
+//  @Override
+//  protected void configure(AuthenticationManagerBuilder auth)
+//      throws Exception {
+//
+//    auth
+//      .userDetailsService(userDetailsService)
+//      .passwordEncoder(encoder());
+//
+//  }
 
 }
