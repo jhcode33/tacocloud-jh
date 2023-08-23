4fabd0e83cbbd5454af4645b741c9708c1015ca4
jhcode33 <jhcode33@gmail.com>
2023-08-23 21:14:35 +0900

    compleate artmis

--- tacocloud-kitchen/src/main/java/tacos/kitchen/WebConfig.java
+++ tacocloud-kitchen/src/main/java/tacos/kitchen/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void addViewControllers(ViewControllerRegistry registry) {
-    registry.addRedirectViewController("/", "/orders/receive");
+    registry.addViewController("/").setViewName("receiveOrder");
   }
   
 }
